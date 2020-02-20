6f692cea6bbc45b3089f4861c583b97889cf8b0c
Tsundzukani <trnghondzweni@gmail.com>
2020-02-20 12:51:37 +0200

    Module packaging at router

--- router/src/main/java/Router.java
+++ router/src/main/java/Router.java
@@ -1,5 +1,15 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class Router {
+    private static ServerSocket broker;
+    private static ServerSocket market;
+
+
+    public static void listenerSocket () throws IOException {
+
+    }
     public static void main(String[] args){
-        System.out.println("Hello From router");
+
     }
 }
